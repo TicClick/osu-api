f47d741cae21034c4948e4ebff328a0f6b430f93
TicClick <ya@ticclick.ch>
2021-05-03 21:47:27 +0200

    add client constructor from token

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
 pub mod api;
+
+use reqwest::{header, blocking};
+
+pub fn new(raw_token: String) -> blocking::Client {
+    let mut headers = header::HeaderMap::new();
+    let mut token = String::from("Bearer ");
+    token.push_str(raw_token.as_str());
+
+    let mut auth = header::HeaderValue::from_str(token.as_str()).unwrap();
+    auth.set_sensitive(true);
+    headers.insert(header::AUTHORIZATION, auth);
+
+    blocking::Client::builder()
+        .default_headers(headers)
+        .build().unwrap()
+}
